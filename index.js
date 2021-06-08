6dbe97735f7b86f43146f7fb3a2fd1baa4f0da53
MattDolly <>
2021-06-08 21:22:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Create an array that can hold 4 items name practiceFile.
+let practiceFile = ["John", "Mary", "Jesus"];
+
+//Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+practiceFile.push("42");
+practiceFile.push("hello");
+
+console.log(practiceFile)
+
+//Use a SetValue to add the items "false", and "-4.6", and “87” to the array.  Print the array to confirm the changes.
